6dcd82b8b98ddec109beb9804afc70e89c679558
Anil Patel <anilpatel9536.2017@gmail.com>
2025-02-15 18:09:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//*const arr = [20,30,12,13,15,17,18]
+// *const arr2 = arr.filter((item,index)=>
+// {
+//     if(item%2==0)
+//         return item
+// })
+// console.log(arr2)//*filter method will return an array 
+//* const arr3 = arr.filter((item,i)=>item%2==0)
+// console.log(arr3)
+
+// let sum =0;
+//* arr.forEach((item,i)=>
+// {
+//     return sum += item;
+// })
+// console.log(sum)//*wont return an array
+
+// *const arr5 = arr.map((item,i)=>
+// {
+//     return item = item + 5
+// })
+// console.log(arr5)
+
+//* const arr6 = arr.reduce((acc,i)=>acc%2==0)
+// console.log(arr6)*/
+
